1b0e4619a45b3b11541559fd492cf4d5d356ed49
Lenk <jmvg.comp@gmail.com>
2019-07-13 00:51:35 -0300

    Validando inconsistências na API de lançamento

--- src/main/java/dev/jmvg/api/exceptionhandler/MyMoneyExceptionHandler.java
+++ src/main/java/dev/jmvg/api/exceptionhandler/MyMoneyExceptionHandler.java
@@ -1,7 +1,9 @@
 package dev.jmvg.api.exceptionhandler;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -51,6 +53,15 @@ public class MyMoneyExceptionHandler extends ResponseEntityExceptionHandler {
         String mensagemDesenvolvedor = ex.toString();
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
         return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+        String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
     }
 
     private List<Erro> criarListaDeErros(BindingResult bindingResult) {

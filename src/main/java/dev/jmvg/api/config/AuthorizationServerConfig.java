0679f245ea990a8c85ec10b74d34659a1883a591
Lenk <jmvg.comp@gmail.com>
2019-07-22 01:27:47 -0300

    Movendo o refresh token para o cookie

--- src/main/java/dev/jmvg/api/config/AuthorizationServerConfig.java
+++ src/main/java/dev/jmvg/api/config/AuthorizationServerConfig.java
@@ -30,21 +30,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret("@ngul@r0")
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(20)
+                .refreshTokenValiditySeconds(3600*24);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter())
+                .reuseRefreshTokens(false)
                 .authenticationManager(authenticationManager);
     }
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey("lenk");
+        accessTokenConverter.setSigningKey("teste");
         return accessTokenConverter;
     }
 
